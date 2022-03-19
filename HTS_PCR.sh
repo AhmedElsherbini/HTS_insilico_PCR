167c4c26554a52e0cd66b38e107174cec58576c4
Ahmed M. A. Elsherbini <49863685+AhmedElsherbini@users.noreply.github.com>
2022-03-19 16:12:54 +0100

    Update HTS_PCR.sh

--- HTS_PCR.sh
+++ HTS_PCR.sh
@@ -1,28 +1,40 @@
-#made on 10 6.12.2021
-echo "Hi, this is Ahmed Elsherbini's HTS-PCR script"
-echo "my work is based on https://github.com/egonozer/in_silico_pcr"
-echo "kindly, have mafft so I can align your amplicon"
-
-echo "So, enough speaking, what is the F primer?"
+#made on 10 1.02.2022
+# "Hi, this is Ahmed Elsherbini's HTS-PCR script"
+echo "this script is based on https://github.com/egonozer/in_silico_pcr.Make sure his perl script and my bash script are in the same dir of the genomes with (.fna extension)"
+echo "ohh,also, have mafft so I can align your amplicon"
+###################################################################
+echo "So, enough speaking, what is the forward primer?"
 read cc
-echo "R primer? y/n"
+echo "Please, The reverse primer?"
 read dd
 
+echo "Allowed mismatch like (2,3,..)?"
+read i
+
+echo "Allowed indels like (2,3,..)?"
+read ii
+#################################################################
 for file in *.fna;
 
 do
 
 #add here you primers and -l max amplicon and which -m mismatch you accept and -i the indels you may accept
-perl in_silico_PCR.pl -r -s $file -a $cc -b $dd -l 100000 -m 4 -i 4 > results_$file.txt 2> amplicons_$file.fasta
+#if you want to a bigger amplicon > 100000 modify this i also, if you want to see which amplicon on which orietation 5 -3 , remove r
+######################################################################################
+perl in_silico_PCR.pl -r -s $file -a $cc -b $dd -l 100000 -m $i -i $ii > results_$file.txt 2> amplicons_$file.fasta
 sed "1s/amp_1/&_${file//./_}/" amplicons_$file.fasta > new_amplicons_$file.fasta
 
 done
 cat new_amplicons* > unaligned_amplicons.fasta
+#if you have too must 
+mafft  --treeout unaligned_amplicons.fasta > aligned_amplicons.fasta
 
-mafft  unaligned_amplicons.fasta > aligned_amplicons.fasta
+
 rm -r results*
 rm -r amplicons*
 rm -r new_amplicons*
 
-echo "See you soon my friend :))"
+mv unaligned_amplicons.fasta.tree phylogentic_amplicons.tree
+
+echo "See you soon my friend (Ahmed):))"
 ##################################################
